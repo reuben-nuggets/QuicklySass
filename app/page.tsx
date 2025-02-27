8196a6b3ed2a9154833900ee25e725f43e0d67ee
reuben-nuggets <jazza.reub@gmail.com>
2025-02-27 16:42:27 +1100

    Added some features and messed around with apis

--- app/page.tsx
+++ app/page.tsx
@@ -1,69 +1,124 @@
 'use client';
 import Image from "next/image";
 import { GoogleGenerativeAI } from "@google/generative-ai";
+import { GetColorName } from 'hex-color-to-color-name';
+import { FormEvent, useState } from 'react';
+import ReactDOM from 'react-dom/client';
 
-let json_data;
+interface company_data {
+	name:string,
+	description:string,
+	primary_color:string,
+	secondary_color:string,
+	niche: string
+}
 
-async function generateCompanyDetails(){
+interface competitor {
+	name:string,
+	url:string,
+	description:string
+}
+
+let company_data:company_data;
+let company_data_string:string;
+let competitors_data:[competitor];
+let competitor_string:string;
+
+function sleep(millisec:number) {
+    return new Promise(resolve => {
+        setTimeout(() => { resolve('') }, millisec);
+    })
+}
+
+
+
+
+async function generateCompanyDetails(userPrompt:string){
 	const genAI = new GoogleGenerativeAI(String(process.env.GEMINI_API_KEY));
 	const model = genAI.getGenerativeModel({
 		model: "gemini-1.5-flash",
 	});
-	const prompt = `Give me details for a company call Quickly SaSS using this json template and include no comments :
+	const prompt = `Give me details for a company called ${userPrompt} using this json template and include no comments also don't use any neutral colours for the primary and scondary colors, a.k.a no gray/black/white. Make sure you never include comments in your json. The niche should be based of the detailed description that you generate :
 	Return: {
+		'niche': string,
 		'name': string,
 		'description': string,
 		'primary_color': string,
 		'secondary_color': string
 	}`;
 	const result = await model.generateContent(prompt);
-	json_data = result.response.text().replaceAll("`", "");
-	json_data = json_data.replaceAll("\n", "");
-	json_data = json_data.slice(4);
-	json_data = JSON.parse(json_data);
-	console.log(json_data);
+	company_data_string = result.response.text().replaceAll("`", "");
+	company_data_string = company_data_string.replaceAll("\n", "");
+	company_data_string = company_data_string.slice(4);
+	company_data = JSON.parse(company_data_string);
+	console.log(company_data);
+	generateCompetitor();
 }
 
-generateCompanyDetails();
+async function generateCompetitor(){
+	const genAI = new GoogleGenerativeAI(String(process.env.GEMINI_API_KEY));
+	const model = genAI.getGenerativeModel({
+		model: "gemini-1.5-flash",
+	});
+	const prompt = `Give me the 5 biggest companies who are like this description of a company ${company_data.description} in plain json format with no comments:
+	Return: {
+		'name': string,
+		'url': string,
+		"description": string
+	}`;
+	const result = await model.generateContent(prompt);
+	competitor_string = result.response.text().replaceAll("`", "");
+	competitor_string = competitor_string.replaceAll("\n", "");
+	competitor_string = competitor_string.slice(4);
+	competitors_data = JSON.parse(competitor_string);
+	console.log(competitors_data);
+}
+
+async function genImage(){
+	const primaryColorName = GetColorName(company_data.primary_color);
+	const secondaryColorName = GetColorName(company_data.secondary_color);
+	const options = {
+		method: 'POST',
+		headers: {
+		  accept: 'application/json',
+		  'content-type': 'application/json',
+		  authorization: 'Bearer key-a4LqqDjW3sLqgNCr4gt42vkTIVfikd9knuXGjVVXWXzYIj6l5FIRMUI7xmCiAouaoRhXGuMbeltfgOAZNdiXvpjhXK7CLnE'
+		},
+		body: JSON.stringify({
+		  prompt: `Give me  a logo for a company called quickly SaSS. Make it have a black background and be simple, using mostly plain geometric shapes. Use the main colour of ${primaryColorName} and if nessecary a secondary colour of ${secondaryColorName} `,
+		  width: 512,
+		  height: 512,
+		  output_format: 'png',
+		  response_format: 'url'
+		})
+	  };
+	  
+	  fetch('https://api.getimg.ai/v1/flux-schnell/text-to-image', options)
+		.then(res => res.json())
+		.then(res => console.log(res))
+		.catch(err => console.error(err));
+}
+
 
 export default function Home() {
+	const [prompt, setPrompt] = useState("");
+	
+	const generateACompanyHandler = (event:FormEvent) => {
+		event.preventDefault();
+		generateCompanyDetails(prompt);
+	}
 
-	function sleep(ms:number) {
-		return new Promise(resolve => setTimeout(resolve, ms));
-	}
-	async function genImage(){
-		const options = {
-			method: 'POST',
-			headers: {
-			  accept: 'application/json',
-			  'content-type': 'application/json',
-			  authorization: 'Bearer key-a4LqqDjW3sLqgNCr4gt42vkTIVfikd9knuXGjVVXWXzYIj6l5FIRMUI7xmCiAouaoRhXGuMbeltfgOAZNdiXvpjhXK7CLnE'
-			},
-			body: JSON.stringify({
-			  prompt: 'Give me  a logo for a company called quickly SaSS. Make it have a transparent background and be simple, using mostly plain geometric shapes. Make sure the background is transparent, not white',
-			  width: 512,
-			  height: 512,
-			  output_format: 'png',
-			  response_format: 'url'
-			})
-		  };
-		  
-		  fetch('https://api.getimg.ai/v1/flux-schnell/text-to-image', options)
-			.then(res => res.json())
-			.then(res => console.log(res))
-			.catch(err => console.error(err));
-	}
 	return (
 		<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-		<h1 className="text-3xl">Hello</h1>
-		<button className="btn btn-primary" onClick={genImage}>Generate Image</button>
-		<div className="flex items-center">
-			<svg className="h-8 w-8 text-blue-500" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-				{/*  Replace this with your actual SVG logo if you have one. */}
-				<path d="M12 2L2 22h20L12 2Z" fill="currentColor"/> {/* Placeholder */}
-			</svg>
-			<span className="text-2xl font-bold text-gray-800 ml-2">quickly SaSS</span>
-		</div>
+			<form onSubmit={generateACompanyHandler}>
+				<label className="px-5">Generate A Company</label>
+				<input value={prompt} onChange={(e) => setPrompt(e.target.value)} className="p-2 bg-black border-2 border-gray-700 rounded-lg" id="genCompanyInput"></input>
+				<input type="submit"></input>
+			</form>
+			<button>Generate Company</button>
+			<h1 className="text-3xl">Hello</h1>
+			<button className="btn btn-primary" onClick={genImage}>Generate Image</button>
+			<button className="btn btn-primary" onClick={generateCompetitor}>Generate Competitors</button>
 		</div>
 	);
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,5 +14,22 @@ export default {
       },
     },
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+            "primary": "#3674B5",
+            "secondary": "#578FCA",
+            "accent": "#A1E3F9",
+            "neutral": "#1e170e",
+            "base-100": "#fbfdf5",
+            "info": "#0093ff",
+            "success": "#099b00",
+            "warning": "#ffa900",
+            "error": "#ff8189",
+          },
+        },
+      ],
+    },
+  plugins: [require('daisyui')],
 } satisfies Config;
